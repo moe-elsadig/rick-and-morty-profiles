46fae9917546ad4dd36ba4e9794ddd3339e887bb
moe-elsadig <m.da7th@gmail.com>
2021-10-12 23:24:32 +0200

    init character list with tests

---
+++ __tests__/components/characterList.test.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import CharacterList from '../../components/CharacterList'
+
+const charactersData =  [{
+  id: 1,
+  name: 'Rick Sanchez',
+  status: 'Alive',
+  species: 'Human',
+  type: '',
+  gender: 'Male',
+  origin: {
+    name: 'Earth (C-137)',
+    url: 'https://rickandmortyapi.com/api/location/1'
+  },
+  location: {
+    name: 'Earth (Replacement Dimension)',
+    url: 'https://rickandmortyapi.com/api/location/20'
+  },
+  image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+  episode: [
+    'https://rickandmortyapi.com/api/episode/1',
+    'https://rickandmortyapi.com/api/episode/2',
+    'https://rickandmortyapi.com/api/episode/3',
+  ],
+  url: 'https://rickandmortyapi.com/api/character/1',
+  created: '2017-11-04T18:48:46.250Z'
+},{
+  id: 1,
+  name: 'Rick Sanchez',
+  status: 'Alive',
+  species: 'Human',
+  type: '',
+  gender: 'Male',
+  origin: {
+    name: 'Earth (C-137)',
+    url: 'https://rickandmortyapi.com/api/location/1'
+  },
+  location: {
+    name: 'Earth (Replacement Dimension)',
+    url: 'https://rickandmortyapi.com/api/location/20'
+  },
+  image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+  episode: [
+    'https://rickandmortyapi.com/api/episode/1',
+    'https://rickandmortyapi.com/api/episode/2',
+    'https://rickandmortyapi.com/api/episode/3',
+  ],
+  url: 'https://rickandmortyapi.com/api/character/1',
+  created: '2017-11-04T18:48:46.250Z'
+},{
+  id: 1,
+  name: 'Rick Sanchez',
+  status: 'Alive',
+  species: 'Human',
+  type: '',
+  gender: 'Male',
+  origin: {
+    name: 'Earth (C-137)',
+    url: 'https://rickandmortyapi.com/api/location/1'
+  },
+  location: {
+    name: 'Earth (Replacement Dimension)',
+    url: 'https://rickandmortyapi.com/api/location/20'
+  },
+  image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+  episode: [
+    'https://rickandmortyapi.com/api/episode/1',
+    'https://rickandmortyapi.com/api/episode/2',
+    'https://rickandmortyapi.com/api/episode/3',
+  ],
+  url: 'https://rickandmortyapi.com/api/character/1',
+  created: '2017-11-04T18:48:46.250Z'
+},{
+  id: 1,
+  name: 'Rick Sanchez',
+  status: 'Alive',
+  species: 'Human',
+  type: '',
+  gender: 'Male',
+  origin: {
+    name: 'Earth (C-137)',
+    url: 'https://rickandmortyapi.com/api/location/1'
+  },
+  location: {
+    name: 'Earth (Replacement Dimension)',
+    url: 'https://rickandmortyapi.com/api/location/20'
+  },
+  image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+  episode: [
+    'https://rickandmortyapi.com/api/episode/1',
+    'https://rickandmortyapi.com/api/episode/2',
+    'https://rickandmortyapi.com/api/episode/3',
+  ],
+  url: 'https://rickandmortyapi.com/api/character/1',
+  created: '2017-11-04T18:48:46.250Z'
+},{
+  id: 1,
+  name: 'Rick Sanchez',
+  status: 'Alive',
+  species: 'Human',
+  type: '',
+  gender: 'Male',
+  origin: {
+    name: 'Earth (C-137)',
+    url: 'https://rickandmortyapi.com/api/location/1'
+  },
+  location: {
+    name: 'Earth (Replacement Dimension)',
+    url: 'https://rickandmortyapi.com/api/location/20'
+  },
+  image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+  episode: [
+    'https://rickandmortyapi.com/api/episode/1',
+    'https://rickandmortyapi.com/api/episode/2',
+    'https://rickandmortyapi.com/api/episode/3',
+  ],
+  url: 'https://rickandmortyapi.com/api/character/1',
+  created: '2017-11-04T18:48:46.250Z'
+}]
+
+describe('Component', () => {
+  it('CharacterList component renders correctly', () => {
+    render(<CharacterList />)
+    const characterList = screen.getByTestId('character-list-component')
+    expect(characterList).toBeTruthy()
+  })
+
+  it('CharacterList renders cards with valid data', ()=>{
+    render(<CharacterList charactersData={charactersData}/>)
+    const profileCardEl0 = screen.getByTestId('profile-card-info-0')
+    const profileCardEl2 = screen.getByTestId('profile-card-info-2')
+    expect(profileCardEl0).toBeTruthy()
+    expect(profileCardEl2).toBeTruthy()
+  })
+
+  it('CharacterList renders loading with invalid data',()=>{
+    render(<CharacterList />)
+    const characterList = screen.getByTestId('character-list-loading')
+    expect(characterList).toBeTruthy()
+  })
+})
---
+++ components/CharacterList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CharacterList() {
+  return <div data-testid="characterList-component">List of characters</div>;
+}
+
+export default CharacterList;
