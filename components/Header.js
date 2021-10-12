d9c3fe4dbcfb02fc680ea683b1dbb22ecab5f36e
moe-elsadig <m.da7th@gmail.com>
2021-10-13 01:40:24 +0200

    styling and sticky behaviour for the header

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,30 @@
 import React from "react";
+import Image from "next/image";
 
 function Header() {
   return (
     <div
       data-testid="Header-component"
-      className="h-[50px] flex flex-row min-w-full shadow-md"
+      className="sticky top-0 z-50 backdrop-blur-3xl h-16 flex flex-row min-w-full shadow-md bg-[#24325f99] dark:bg-[#24325fff]"
     >
       <div className="m-auto flex items-center">
-        <h1 className="" data-testid="Header-heading">
-          Rick & Morty Encyclopedia
+        <div className="flex w-14 h-14 relative flex-shrink-0 m-auto">
+          <Image
+            data-testid="Header-logo"
+            src="/logo.png"
+            layout="fill"
+            objectFit="contain"
+            className=""
+          />
+        </div>
+        <h1
+          className="text-4xl font-bold text-gray-800 dark:text-gray-100"
+          data-testid="Header-heading"
+        >
+          The <span className="text-blue-400 dark:text-blue-500">Rick</span>{" "}
+          <span className="text-green-400 dark:text-green-500">&</span>{" "}
+          <span className="text-yellow-400 dark:text-yellow-500">Morty</span>{" "}
+          Encyclopedia
         </h1>
         {/* TODO: logo */}
       </div>
