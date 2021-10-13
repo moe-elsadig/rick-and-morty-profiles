68778005fe7d6befc7e40fe9bb8bd7676386fd01
moe-elsadig <m.da7th@gmail.com>
2021-10-14 00:45:19 +0200

    styling and theme change button

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Link from "next/link";
 import Image from "next/image";
 import { MdLightMode, MdNightlight } from "react-icons/md";
 
@@ -8,21 +9,23 @@ function Header({ changeTheme }) {
   return (
     <div
       data-testid="Header-component"
-      className="sticky top-0 z-50 bg-white dark:bg-black border-b border-blue-300 h-16 flex flex-row w-screen shadow-md bg-[#24325f99] dark:bg-[#24325fff]"
+      className="sticky top-0 z-50 bg-white dark:bg-gray-800 h-16 flex flex-row w-screen shadow-md items-center"
     >
-      <div className="flex w-14 h-14 relative flex-shrink-0 m-auto">
-        <Image
-          data-testid="Header-logo"
-          src="/logo.png"
-          layout="fill"
-          objectFit="contain"
-          className=""
-        />
-      </div>
+      <Link href="/">
+        <div className="flex w-14 h-14 relative flex-shrink-0 m-auto pl-16 cursor-pointer">
+          <Image
+            data-testid="Header-logo"
+            src="/logo.png"
+            layout="fill"
+            objectFit="contain"
+            className=""
+          />
+        </div>
+      </Link>
 
-      <div className="flex-shrink">
+      <div className="flex-shrink pl-2">
         <h1
-          className="text-4xl font-bold text-gray-800 dark:text-gray-100"
+          className="hidden md:inline-flex md:text-2xl lg:text-3xl xl:text-4xl font-bold text-gray-800 dark:text-gray-100"
           data-testid="Header-heading"
         >
           The <span className="text-blue-400 dark:text-blue-500">Rick</span>{" "}
@@ -34,35 +37,21 @@ function Header({ changeTheme }) {
 
       <div className="flex-grow"></div>
 
-      <div className="flex-shrink-0"></div>
+      <div className="flex-shrink-0 px-4" data-testid="Header-buttons-area">
+        <button
+          onClick={() => {
+            setTheme(!theme);
+            changeTheme();
+          }}
+          className={`px-5 py-2 rounded-2xl opacity-70 transition transform duration-200 ease-out hover:shadow-lg hover:scale-105 hover:opacity-100 hover:animate-pulse`}
+        >
+          <p className="font-semibold text-black dark:text-white">
+            {theme ? <MdLightMode /> : <MdNightlight />}
+          </p>
+        </button>
+      </div>
     </div>
   );
 }
 
 export default Header;
-
-{
-  /* <div className="m-auto flex items-center">
-<div className="flex w-14 h-14 relative flex-shrink-0 m-auto">
-  <Image
-    data-testid="Header-logo"
-    src="/logo.png"
-    layout="fill"
-    objectFit="contain"
-    className=""
-  />
-</div>
-<h1
-  className="text-4xl font-bold text-gray-800 dark:text-gray-100"
-  data-testid="Header-heading"
->
-  The <span className="text-blue-400 dark:text-blue-500">Rick</span>{" "}
-  <span className="text-green-400 dark:text-green-500">&</span>{" "}
-  <span className="text-yellow-400 dark:text-yellow-500">Morty</span>{" "}
-  Encyclopedia
-</h1>
-</div>
-<div data-testid="Header-buttons-area bg-yellow-300">
-<MdLightMode className="" />
-</div> */
-}
