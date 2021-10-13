8f0e39cc3eeb95525f8b4ddc4854d6b70df9a311
moe-elsadig <m.da7th@gmail.com>
2021-10-13 17:56:01 +0200

    added basic pagination

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,79 @@
 import CharacterList from "@/components/CharacterList";
 import Header from "@/components/Header";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import { getCharacters } from "rickmortyapi";
 
-export default function Home({ characterData }) {
-  let cardsMarkup =
-    characterData?.status === 200 ? (
-      <CharacterList charactersData={characterData?.data?.results} />
-    ) : (
-      <div className="flex flex-1">
-        <p className="m-auto">Data loading...</p>
-      </div>
-    );
+const startingPage = 1;
+const defaultEndpoint = "https://rickandmortyapi.com/api/character/?page=1";
+
+export default function Home({ data }) {
+  console.log(data);
+
+  const { info, results: newResults = [] } = data;
+  const [results, setResults] = useState(newResults);
+  const [page, setPage] = useState({
+    ...info,
+    currentPageNo: startingPage,
+    currentPage: defaultEndpoint,
+  });
+
+  const { currentPage, currentPageNo } = page;
+
+  useEffect(() => {
+    console.log("effect:", currentPage, currentPageNo);
+    // if (currentPageNo === startingPage) return;
+    // if (currentPage === defaultEndpoint) return;
+
+    async function getNewData() {
+      console.log("request new data");
+      // use the installed API
+      const res = await getCharacters({ page: currentPageNo });
+      const data = res.data;
+
+      // use the online REST API
+      // const data = await (await fetch(currentPage)).json();
+
+      console.log("set new data", data);
+      setResults(data.results);
+      setPage((prev) => {
+        return {
+          ...prev,
+          ...data.info,
+        };
+      });
+    }
+
+    getNewData();
+  }, [currentPage, currentPageNo]);
+
+  let nextPage = () => {
+    setPage((prev) => {
+      return {
+        ...prev,
+        currentPage: prev.next,
+        currentPageNo: prev.currentPageNo + 1,
+      };
+    });
+  };
+
+  let prevPage = () => {
+    setPage((prev) => {
+      return {
+        ...prev,
+        currentPage: prev.prev,
+        currentPageNo: prev.currentPageNo - 1,
+      };
+    });
+  };
+
+  let cardsMarkup = results ? (
+    <CharacterList charactersData={results} />
+  ) : (
+    <div className="flex flex-1">
+      <p className="m-auto">Data loading...</p>
+    </div>
+  );
 
   return (
     <div className="dark">
@@ -24,15 +86,27 @@ export default function Home({ characterData }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className="flex min-h-screen">{cardsMarkup}</main>
+      <main className="flex flex-col min-h-screen">
+        {cardsMarkup}
+        <div className="m-auto">
+          <button onClick={prevPage}>Back</button>
+          {"  "}
+          <button onClick={nextPage}>Next</button>
+        </div>
+      </main>
     </div>
   );
 }
 
 export async function getServerSideProps() {
-  const characterData = await getCharacters({ page: 1 });
+  // use the installed API
+  const res = await getCharacters({ page: startingPage });
+  const data = res.data;
+
+  // use the online REST API
+  // const data = await (await fetch(defaultEndpoint)).json();
 
   return {
-    props: { characterData },
+    props: { data },
   };
 }
