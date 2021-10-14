671d1478865525b6b32f5deec7ed93ad983fcaae
moe-elsadig <m.da7th@gmail.com>
2021-10-14 23:15:35 +0200

    link the episode buttons

--- components/CharacterProfile.js
+++ components/CharacterProfile.js
@@ -140,14 +140,20 @@ function CharacterProfile({ characterData }) {
 
   let episodesMarkup = characterData
     ? characterData.episode.map((episode, index) => (
-        <button
+        <Link
+          href={episode.replace("https://rickandmortyapi.com/api", "")}
           key={index}
-          className={`w-32 bg-yellow-400 dark:bg-yellow-700 px-5 py-2 rounded-2xl shadow-lg hover:scale-105 transition transform duration-200 ease-out hover:animate-pulse font-semibold text-black dark:text-white`}
         >
-          <p className="">
-            Episode {episode.split("https://rickandmortyapi.com/api/episode/")}
-          </p>
-        </button>
+          <button
+            key={index}
+            className={`w-32 bg-yellow-400 dark:bg-yellow-700 px-5 py-2 rounded-2xl shadow-lg hover:scale-105 transition transform duration-200 ease-out hover:animate-pulse font-semibold text-black dark:text-white`}
+          >
+            <p className="">
+              Episode{" "}
+              {episode.split("https://rickandmortyapi.com/api/episode/")}
+            </p>
+          </button>
+        </Link>
       ))
     : "Loading episodes...";
 
