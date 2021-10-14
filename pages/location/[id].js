d67f3d0d8c32a8d74e5c4815c4fdc5ce56ca1120
moe-elsadig <m.da7th@gmail.com>
2021-10-14 21:46:55 +0200

    add styling and resident routes to the profile

--- pages/location/[id].js
+++ pages/location/[id].js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
-import CharacterProfile from "@/components/CharacterProfile";
+import LocationProfile from "@/components/LocationProfile";
 import ProfileCard from "@/components/ProfileCard";
 import Header from "@/components/Header";
 
@@ -22,7 +22,7 @@ function LocationPage({ data }) {
   return (
     <div className={`${theme && "dark"} bg-gray-500`}>
       <Head>
-        <title>{data?.name}&#39;s Profile</title>
+        <title>{data?.name} - Location</title>
         <meta
           name="description"
           content={`${data?.id}-${data?.name}-Rick & Morty Encyclopedia powere by the rickandmortyapi.com API`}
@@ -31,7 +31,7 @@ function LocationPage({ data }) {
       </Head>
       <Header changeTheme={changeTheme} />
       <main className="flex flex-col min-h-screen h-full items-center p-10 bg-gray-100 dark:bg-gray-900">
-        <CharacterProfile characterData={data} />
+        <LocationProfile locationData={data} />
       </main>
     </div>
   );
@@ -43,7 +43,7 @@ export async function getServerSideProps({ query }) {
   const { id } = query;
 
   // use the online REST API
-  const characterEndpoint = `https://rickandmortyapi.com/api/character/${id}`;
+  const characterEndpoint = `https://rickandmortyapi.com/api/location/${id}`;
   const data = await (await fetch(characterEndpoint)).json();
 
   return {
