3d1229001770f6e7cc61378e1cf7bbfee8879e0c
moe-elsadig <m.da7th@gmail.com>
2021-10-17 02:52:28 +0200

    update header tests

--- __tests__/components/header.test.jsx
+++ __tests__/components/header.test.jsx
@@ -3,19 +3,26 @@ import { render, screen } from '@testing-library/react'
 import Header from '../../components/Header'
 
 describe('Component', () => {
-  it('Header component renders correctly', () => {
-    render(<Header />)
-    const header = screen.getAllByTestId('Header-component')
-    expect(header).toBeTruthy()
+  it('Header component renders', () => {
+    let component = render(<Header />)
+    const headerEl = component.getByTestId('Header-component')
+    expect(headerEl).toBeTruthy()
   })
 
   it('Header component Heading renders correctly', () => {
     render(<Header />)
-    const heading = screen.getAllByTestId('Header-heading')
-    expect(heading).toBeTruthy()
+    const heading = screen.getByTestId('Header-heading')
+    expect(heading.textContent).toBe('The Rick & Morty Encyclopedia')
   })
 
-  it('Header component buttons area renders correctly', () => {
+  it('Header component Logo renders correctly', () => {
+    render(<Header />)
+    const heading = screen.getByTestId('Header-heading')
+    expect(heading.textContent).toBe('The Rick & Morty Encyclopedia')
+  })
+
+
+  it('Header component buttons area renders', () => {
     render(<Header />)
     const buttons = screen.getAllByTestId('Header-buttons-area')
     expect(buttons).toBeTruthy()
