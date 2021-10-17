1bcd97a3d20b510c4b9c9f6310b6635d192d6354
moe-elsadig <m.da7th@gmail.com>
2021-10-17 18:35:08 +0200

    additional tests

--- components/Paginator.js
+++ components/Paginator.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-function Paginator({ nextFn, prevFn, currentPage, maxPage }) {
+function Paginator({ nextFn, prevFn, currentPage = 1, maxPage }) {
   return (
-    <div data-testid="paginator-buttons-area" className="grid grid-cols-3 py-5">
+    <div data-testid="paginator-component" className="grid grid-cols-3 py-5">
       <button
+        data-testid="paginator-back"
         onClick={prevFn}
         disabled={currentPage === 1 ? true : false}
         className={`bg-yellow-300 dark:bg-yellow-700 hover:bg-yellow-200 dark:hover:bg-yellow-600 px-5 py-2 rounded-2xl shadow-lg hover:scale-105 transition transform duration-200 ease-out hover:animate-pulse font-semibold text-black dark:text-white ${
@@ -14,9 +15,10 @@ function Paginator({ nextFn, prevFn, currentPage, maxPage }) {
         <p className="">Back</p>
       </button>
       <div className="font-bold m-auto text-black dark:text-white pb-2 border-b border-blue-300 darker:border-blue-600">
-        <p>{currentPage}</p>
+        <p data-testid="paginator-number">{currentPage}</p>
       </div>
       <button
+        data-testid="paginator-next"
         onClick={nextFn}
         disabled={currentPage === maxPage ? true : false}
         className={`bg-yellow-300 dark:bg-yellow-700 hover:bg-yellow-200 dark:hover:bg-yellow-600 px-5 py-2 rounded-2xl shadow-lg hover:scale-105 transition transform duration-200 ease-out hover:animate-pulse ${
