08bbd40dd23679818afdbd23e35bf877cf1bb200
moe-elsadig <m.da7th@gmail.com>
2021-10-27 20:51:29 +0200

    add loading screen content

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 function LoadingScreen() {
-  return <div></div>;
+  return (
+    <div className="h-screen w-screen flex flex-grow items-center justify-center m-auto bg-gray-200 dark:bg-gray-700">
+      <Image
+        src="/portal.gif"
+        alt="loading image"
+        height={200}
+        width={200}
+        className="animate-pulse"
+        priority={true}
+      />
+    </div>
+  );
 }
 
 export default LoadingScreen;
