d8c26e84312444988201e6eec177c3191daf3d41
moe-elsadig <m.da7th@gmail.com>
2021-10-30 03:31:34 +0200

    remove loader

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,6 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import ProgressBar from "@badrap/bar-of-progress";
 import Router from "next/router";
-import LoadingScreen from "@/components/LoadingScreen";
-import { Fragment, useEffect, useState } from "react";
 
 const progress = new ProgressBar({
   size: 4,
@@ -17,24 +15,7 @@ Router.events.on("routeChangeComplete", progress.finish);
 Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => setLoading(false), 2000);
-  }, []);
-
-  return (
-    <>
-      {!loading ? (
-        <Fragment>
-          <Component {...pageProps} />
-        </Fragment>
-      ) : (
-        <LoadingScreen />
-      )}
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
