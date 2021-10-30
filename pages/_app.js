516b193802991a96cf410b175d31b19bc68efac7
moe-elsadig <m.da7th@gmail.com>
2021-10-30 02:42:36 +0200

    add loading screen

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import ProgressBar from "@badrap/bar-of-progress";
 import Router from "next/router";
+import LoadingScreen from "@/components/LoadingScreen";
+import { Fragment, useEffect, useState } from "react";
 
 const progress = new ProgressBar({
   size: 4,
@@ -15,7 +17,24 @@ Router.events.on("routeChangeComplete", progress.finish);
 Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 2000);
+  }, []);
+
+  return (
+    <>
+      {!loading ? (
+        <Fragment>
+          <Component {...pageProps} />
+        </Fragment>
+      ) : (
+        <LoadingScreen />
+      )}
+    </>
+  );
 }
 
 export default MyApp;
