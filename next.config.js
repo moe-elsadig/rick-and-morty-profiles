ae4cf2395989fbc6826436fcf26565ce6bed44d1
moe-elsadig <m.da7th@gmail.com>
2021-11-01 23:37:24 +0200

    add pwa logic and service worker

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ["rickandmortyapi.com"],
   },
   // swcMinify: true,
-};
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    // disable: process.env.NODE_ENV !== "production",
+  },
+});
